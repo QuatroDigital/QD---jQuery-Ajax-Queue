ae949905764e7aa6c4005b63930a6f16c64b9065
Carlos Vinicius <carlosjp@hotmail.co.jp>
2013-04-03 15:10:02 -0300

    primeiro commit

---
+++ QD-jQueryAjaxQueue.js
@@ -0,0 +1,85 @@
+/**
+ * Quatro Digital - jQuery Ajax Queue
+ * @author Carlos Vinicius [ QUATRO DIGITAL ]
+ * @version 1.0
+ * @license MIT <http://pt.wikipedia.org/wiki/Licen%C3%A7a_MIT>
+*/
+(function($){
+	if(typeof $.qdAjax === "function") return;
+
+	// Aramazenamento da fila
+	$.qdAjaxQueue={};
+	
+	// Plugin
+	$.qdAjax=function(opts){
+		var defaults,options,urlId,queue,ajaxOptions;
+		
+		defaults={
+			success:function(){},
+			error:function(){},
+			complete:function(){},
+			clearQueueDelay:0
+		};
+		options=$.extend({},defaults, opts);
+	
+		// Adicionando a requisição na fila
+		urlId=escape(encodeURIComponent(options.url));
+		queue={
+			success:function(data, textStatus, jqXHR){
+				options.success.call(this, data, textStatus, jqXHR);
+			},
+			error:function(jqXHR, textStatus, errorThrown){
+				options.error.call(this, jqXHR, textStatus, errorThrown);
+			},
+			complete:function(jqXHR, textStatus){
+				options.complete.call(this, jqXHR, textStatus);
+			}
+		};
+		// Adicionando um novo item a fila
+		$.qdAjaxQueue[urlId]=$.qdAjaxQueue[urlId]||{};
+		$.qdAjaxQueue[urlId]["opts"]=$.qdAjaxQueue[urlId]["opts"]||[];
+		$.qdAjaxQueue[urlId]["opts"].push(queue);
+		
+		// Criando função que chamará a fila
+		ajaxOptions=$.extend({},options,{
+			success:function(data, textStatus, jqXHR){
+				// var item;
+				for(var i in $.qdAjaxQueue[urlId]["opts"]){
+					if(typeof $.qdAjaxQueue[urlId]["opts"][i] !== "object")
+						continue;
+
+					$.qdAjaxQueue[urlId]["opts"][i].success.call(this, data, textStatus, jqXHR);
+					$.qdAjaxQueue[urlId]["opts"][i].success=function(){};
+				}
+			},
+			error:function(jqXHR, textStatus, errorThrown){
+				for(var i in $.qdAjaxQueue[urlId]["opts"]){
+					if(typeof $.qdAjaxQueue[urlId]["opts"][i] !== "object")
+						continue;
+
+					$.qdAjaxQueue[urlId]["opts"][i].error.call(this, jqXHR, textStatus, errorThrown);
+					$.qdAjaxQueue[urlId]["opts"][i].error=function(){};
+				}
+			},
+			complete:function(jqXHR, textStatus){
+				for(var i in $.qdAjaxQueue[urlId]["opts"]){
+					if(typeof $.qdAjaxQueue[urlId]["opts"][i] !== "object")
+						continue;
+
+					$.qdAjaxQueue[urlId]["opts"][i].complete.call(this, jqXHR, textStatus);
+					$.qdAjaxQueue[urlId]["opts"][i].complete=function(){};
+				}
+				
+				// Removendo os dados jqXHR
+				setTimeout(function(){
+					$.qdAjaxQueue[urlId]["jqXHR"]=undefined;
+					$.qdAjaxQueue[urlId]["opts"]=undefined;
+				},options.clearQueueDelay);
+			}
+		});
+		
+		// Fazendo a chamada Ajax
+		if(typeof $.qdAjaxQueue[urlId]["jqXHR"] === "undefined")
+			$.qdAjaxQueue[urlId]["jqXHR"]=$.ajax(ajaxOptions);
+	};
+})(jQuery);
---
+++ README.md
@@ -0,0 +1,13 @@
+#QUATRO DIGITAL - jQuery Ajax Queue#
+
+###Atenção
+> Esta extensão é mantida por [Quatro Digital](http://www.quatrodigital.com.br) e não possui suporte gratuito.  
+> O código fonte deste componente não pode ser vendido ou comercializado, ele esta livre para uso comercial mas só podem haver cobranças com relação à mão de obra necessária a sua instalação e não por sua utilização.  
+> O correto funcionamento deste script não é de responsabilidade de seu desenvolvedor, mantenedor ou constribuidores.  
+> Caso queira contribuir com o desenvolvimento fique a vontade para fazer um `Fork` e posteriormente um `pull request`.
+
+**O uso desta extensão esta sob as regras da lincença: [MIT](http://pt.wikipedia.org/wiki/Licen%C3%A7a_MIT)**
+
+----------
+
+No momento não existe documentação para este plugin
